a5045d50fda0a725ecbef2cd5ba0d700a157e65e
Syamlal CM <syamjayaraj@gmail.com>
2018-02-15 11:31:04 +0530

    Add contact modal added.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import React, { Component } from 'react';
-import AddContact from './AddContact';
+
 import SearchBar from './SearchBar';
 import ContactCard from './ContactCard';
 
-import { Navbar, Nav, NavDropdown, MenuItem, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, MenuItem, ListGroup, ListGroupItem, Button, Modal, Col, Glyphicon, Form, FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
 
 const contactsAPI = 'https://demo1443058.mockable.io/codeproject_tutorial/api/contacts';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
     this.state = {
       searchText: '',
       searchResult: [],
-      contactList: []
+      contactList: [],
+      show: false,
     }
     this.handleSearch = this.handleSearch.bind(this);
     this.returnContactList = this.returnContactList.bind(this);
+  }
+
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
   }
 
 
@@ -83,8 +97,77 @@ class App extends Component {
         </Navbar>
       <br /> <br /> <br />
       <div className="container">
-        <div className="col-lg-8 col-lg-offset-2"> 
-          <AddContact />
+        <div className="col-lg-8 col-lg-offset-2">
+
+        <div>
+        <Col lg={4} lgOffset={11}>
+            <div className=" input-group-lg">
+                <Button bsStyle="default" bsSize="large" onClick={this.handleShow}>
+                    <Glyphicon glyph="glyphicon glyphicon-plus" />
+                </Button>
+            </div>
+            <br />
+        </Col>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Add contact</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+
+          <Form horizontal>
+            <FormGroup controlId="formHorizontalEmail">
+              <Col componentClass={ControlLabel} sm={2}>
+                Name
+              </Col>
+              <Col sm={10}>
+                <FormControl type="text" placeholder="Name" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalPassword">
+              <Col componentClass={ControlLabel} sm={2}>
+                Mobile
+              </Col>
+              <Col sm={10}>
+                <FormControl type="text" placeholder="Mobile" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalPassword">
+              <Col componentClass={ControlLabel} sm={2}>
+                Email
+              </Col>
+              <Col sm={10}>
+                <FormControl type="email" placeholder="Email" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup controlId="formHorizontalPassword">
+              <Col componentClass={ControlLabel} sm={2}>
+                Address
+              </Col>
+              <Col sm={10}>
+                <FormControl componentClass="textarea" placeholder="Address" />
+              </Col>
+            </FormGroup>
+
+            <FormGroup>
+              <Col smOffset={2} sm={10}>
+                <Button type="submit">Save</Button>
+              </Col>
+            </FormGroup>
+          </Form>
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleClose}>Cancel</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+
+
+  
           <br />       
           <SearchBar onSearch={this.handleSearch} />
           <br />
